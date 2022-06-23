ce18d99e26e67ddbd42a0294c8bf81cdc0af21b7
ashmik <ashmik.harinkhede@gmail.com>
2022-06-23 12:52:29 +0530

    Return Transaction and Fine calculation

--- src/main/java/com/example/jbdl/minorproject1/services/TransactionService.java
+++ src/main/java/com/example/jbdl/minorproject1/services/TransactionService.java
@@ -5,14 +5,23 @@ import com.example.jbdl.minorproject1.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class TransactionService {
 
     @Value("${student.book.quota}")
     int studentBookQuota;
+
+    @Value("${book.return.days}")
+    int bookReturnDays;
+
+    @Value("${book.fine.day}")
+    int finePerDay;
 
     @Autowired
     StudentService studentService;
@@ -90,7 +99,87 @@ public class TransactionService {
         return transaction.getTransactionId();
     }
 
+
     public String returnBook(int bookId, int studentId) throws Exception{
-        return null;
+        /**
+         * 1. Check whether the book exists or not
+         * 2. Check for fine and update it
+         * 3. Create a txn with pending status
+         * 4. Make the book available || Remove the student as assignee
+         * 5. Update the txn with success status
+         **/
+
+        // 1. Fetching book, student
+        Student student = studentService.getStudentById(studentId);
+        Book book = bookService.getBookById(bookId);
+
+        // Validations
+        if (student == null || book == null) {
+            throw new Exception("Either student or book does not exist, Unable to return");
+        }
+
+        if (book.getStudent() == null || book.getStudent().getId() != studentId) {
+            throw new Exception("Book is not assigned to the student, Unable to return");
+        }
+
+        // 2. Calculate Fine
+        // Now, for further processing
+        // We need to retrieve the corresponding issue transaction for this return (Since it will have the updateDate which is needed to calculate the fine)
+
+        List<Transaction> txnList = transactionRepository
+                .findByBookAndStudentAndTransactionTypeOrderByIdDesc(book,
+                        student, TransactionType.ISSUE);
+
+        Transaction issueTxn = txnList.get(0);
+
+//        Transaction issueTxn = transactionRepository
+//                .findTopByBookAndStudentAndTransactionTypeOrderByIdDesc(
+//                bookId, studentId, TransactionType.ISSUE
+//        );
+
+        // Tracking days taken to return and calculating fine
+
+        long issueTimeInMillis = issueTxn.getUpdatedOn().getTime();
+        long currentTimeInMillis = System.currentTimeMillis();
+
+        long timeDiff = currentTimeInMillis - issueTimeInMillis;
+        long numberOfDaysPassed = TimeUnit.DAYS.convert(timeDiff, TimeUnit.MILLISECONDS);
+
+        int fine = 0;
+        if (numberOfDaysPassed > bookReturnDays) {
+            int extraDays = (int) (numberOfDaysPassed - bookReturnDays);
+            fine = extraDays * finePerDay;
+        }
+
+        // Creating the return transaction now
+        Transaction transaction = Transaction.builder()
+                .book(book)
+                .student(student)
+                .fine(fine)
+                .transactionStatus(TransactionStatus.PENDING)
+                .transactionType(TransactionType.RETURN)
+                .transactionId(UUID.randomUUID().toString())
+                .build();
+
+        transactionRepository.save(transaction);
+
+
+        try{
+
+            book.setStudent(null);
+            bookService.insert(book);
+
+            transaction.setTransactionStatus(TransactionStatus.SUCCESS);
+            transactionRepository.save(transaction);
+
+        } catch(Exception e) {
+            book.setStudent(student);
+            bookService.insert(book);
+
+            transaction.setTransactionStatus(TransactionStatus.FAILED);
+            transactionRepository.save(transaction);
+        }
+
+        return transaction.getTransactionId();
     }
 }
